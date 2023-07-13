f9da32c06c929e4eed527c9231588231559ab879
Niyaz Fazlyev <>
2023-07-13 22:49:02 +0300

    refactoring settings page

---
+++ lib/purchasing/paywalls/paywall_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PaywallPage extends StatelessWidget {
+  const PaywallPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
